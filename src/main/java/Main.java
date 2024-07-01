3f58318f0c51e31be20b4f7cf061ca326c9c3a17
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-07-01 16:31:28 +0700

    create 3 queues

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,23 @@
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Main {
+
+    static BlockingQueue<String> aQueue = new ArrayBlockingQueue<>(100);
+    static BlockingQueue<String> bQueue = new ArrayBlockingQueue<>(100);
+    static BlockingQueue<String> cQueue = new ArrayBlockingQueue<>(100);
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
 }
